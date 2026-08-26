8c3a588eb9e9ae8dd5ff3bf0258fd8989e32e945
JEFFERSON ENRIQUE DE LA CRUZ MORALES <USSS128826@ugb.edu.sv>
2026-08-26 09:59:28 -0600

    Add project files.

---
+++ Conversores/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Conversores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escriba cantidad";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(141, 98);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(98, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(360, 98);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(99, 24);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(141, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Convertir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(420, 17);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(341, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Categoria ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "De";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(289, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "a";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Conversores/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Conversores
+{
+    public partial class Form1 : Form
+    {
+        public class UnidadMedida
+        {
+            public string Nombre { get; set; }
+            public double FactorABase { get; set; }
+        }
+
+        private Dictionary<string, List<UnidadMedida>> categorias;
+        private Label lblResultado;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CrearLabelResultado();
+            CargarDatos();
+        }
+
+        private void CrearLabelResultado()
+        {
+           
+            lblResultado = new Label();
+            lblResultado.Location = new Point(30, 200);
+            lblResultado.Size = new Size(430, 30);
+            lblResultado.Font = new Font("Arial", 10, FontStyle.Bold);
+            lblResultado.ForeColor = Color.DarkBlue;
+            lblResultado.TextAlign = ContentAlignment.MiddleCenter;
+            this.Controls.Add(lblResultado);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            comboBox3.DataSource = categorias.Keys.ToList();
+            comboBox3.SelectedIndex = 0;
+
+            
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
+
+            ActualizarCombosUnidades();
+        }
+
+        private void CargarDatos()
+        {
+            categorias = new Dictionary<string, List<UnidadMedida>>
+            {
+                ["Monedas"] = new List<UnidadMedida>
+                {
+                    new UnidadMedida { Nombre = "Dólar estadounidense (USD)", FactorABase = 1.0 },
+                    new UnidadMedida { Nombre = "Euro (EUR)", FactorABase = 1.08 },
+                    new UnidadMedida { Nombre = "Libra esterlina (GBP)", FactorABase = 1.27 },
+                    new UnidadMedida { Nombre = "Yen japonés (JPY)", FactorABase = 0.0067 },
+                    new UnidadMedida { Nombre = "Dólar canadiense (CAD)", FactorABase = 0.74 },
+                    new UnidadMedida { Nombre = "Peso mexicano (MXN)", FactorABase = 0.058 },
+                    new UnidadMedida { Nombre = "Quetzal guatemalteco (GTQ)", FactorABase = 0.13 },
+                    new UnidadMedida { Nombre = "Lempira hondureño (HNL)", FactorABase = 0.040 },
+                    new UnidadMedida { Nombre = "Córdoba nicaragüense (NIO)", FactorABase = 0.027 },
+                    new UnidadMedida { Nombre = "Colón costarricense (CRC)", FactorABase = 0.0019 }
+                },
+                ["Masa"] = new List<UnidadMedida>
+                {
+                    new UnidadMedida { Nombre = "Kilogramo (kg)", FactorABase = 1.0 },
+                    new UnidadMedida { Nombre = "Gramo (g)", FactorABase = 0.001 },
+                    new UnidadMedida { Nombre = "Miligramo (mg)", FactorABase = 0.000001 },
+                    new UnidadMedida { Nombre = "Libra (lb)", FactorABase = 0.453592 },
+                    new UnidadMedida { Nombre = "Onza (oz)", FactorABase = 0.0283495 },
+                    new UnidadMedida { Nombre = "Tonelada métrica (t)", FactorABase = 1000.0 },
+                    new UnidadMedida { Nombre = "Arroba (@)", FactorABase = 11.3398 },
+                    new UnidadMedida { Nombre = "Quintal (qq)", FactorABase = 45.3592 },
+                    new UnidadMedida { Nombre = "Grano (gr)", FactorABase = 0.0000647989 },
+                    new UnidadMedida { Nombre = "Stone (st)", FactorABase = 6.35029 }
+                },
+                ["Volumen"] = new List<UnidadMedida>
+                {
+                    new UnidadMedida { Nombre = "Litro (L)", FactorABase = 1.0 },
+                    new UnidadMedida { Nombre = "Mililitro (mL)", FactorABase = 0.001 },
+                    new UnidadMedida { Nombre = "Metro cúbico (m³)", FactorABase = 1000.0 },
+                    new UnidadMedida { Nombre = "Galón (US gal)", FactorABase = 3.78541 },
+                    new UnidadMedida { Nombre = "Cuarto de galón (qt)", FactorABase = 0.946353 },
+                    new UnidadMedida { Nombre = "Pinta (pt)", FactorABase = 0.473176 },
+                    new UnidadMedida { Nombre = "Taza (cup)", FactorABase = 0.24 },
+                    new UnidadMedida { Nombre = "Onza líquida (fl oz)", FactorABase = 0.0295735 },
+                    new UnidadMedida { Nombre = "Cucharada (tbsp)", FactorABase = 0.0147868 },
+                    new UnidadMedida { Nombre = "Cucharadita (tsp)", FactorABase = 0.00492892 }
+                },
+                ["Longitud"] = new List<UnidadMedida>
+                {
+                    new UnidadMedida { Nombre = "Metro (m)", FactorABase = 1.0 },
+                    new UnidadMedida { Nombre = "Kilómetro (km)", FactorABase = 1000.0 },
+                    new UnidadMedida { Nombre = "Centímetro (cm)", FactorABase = 0.01 },
+                    new UnidadMedida { Nombre = "Milímetro (mm)", FactorABase = 0.001 },
+                    new UnidadMedida { Nombre = "Milla (mi)", FactorABase = 1609.34 },
+                    new UnidadMedida { Nombre = "Yarda (yd)", FactorABase = 0.9144 },
+                    new UnidadMedida { Nombre = "Pie (ft)", FactorABase = 0.3048 },
+                    new UnidadMedida { Nombre = "Pulgada (in)", FactorABase = 0.0254 },
+                    new UnidadMedida { Nombre = "Milla náutica (nmi)", FactorABase = 1852.0 },
+                    new UnidadMedida { Nombre = "Micrómetro (µm)", FactorABase = 0.000001 }
+                },
+                ["Almacenamiento"] = new List<UnidadMedida>
+                {
+                    new UnidadMedida { Nombre = "Megabyte (MB)", FactorABase = 1.0 },
+                    new UnidadMedida { Nombre = "Bit (b)", FactorABase = 0.00000011920928955078125 },
+                    new UnidadMedida { Nombre = "Byte (B)", FactorABase = 0.00000095367431640625 },
+                    new UnidadMedida { Nombre = "Kilobyte (KB)", FactorABase = 0.0009765625 },
+                    new UnidadMedida { Nombre = "Gigabyte (GB)", FactorABase = 1024.0 },
+                    new UnidadMedida { Nombre = "Terabyte (TB)", FactorABase = 1048576.0 },
+                    new UnidadMedida { Nombre = "Petabyte (PB)", FactorABase = 1073741824.0 },
+                    new UnidadMedida { Nombre = "Nibble", FactorABase = 0.000000476837158203125 },
+                    new UnidadMedida { Nombre = "Kibibyte (KiB)", FactorABase = 0.001 },
+                    new UnidadMedida { Nombre = "Mebibyte (MiB)", FactorABase = 1.048576 }
+                },
+                ["Tiempo"] = new List<UnidadMedida>
+                {
+                    new UnidadMedida { Nombre = "Segundo (s)", FactorABase = 1.0 },
+                    new UnidadMedida { Nombre = "Milisegundo (ms)", FactorABase = 0.001 },
+                    new UnidadMedida { Nombre = "Minuto (min)", FactorABase = 60.0 },
+                    new UnidadMedida { Nombre = "Hora (h)", FactorABase = 3600.0 },
+                    new UnidadMedida { Nombre = "Día (d)", FactorABase = 86400.0 },
+                    new UnidadMedida { Nombre = "Semana", FactorABase = 604800.0 },
+                    new UnidadMedida { Nombre = "Mes (30 días)", FactorABase = 2592000.0 },
+                    new UnidadMedida { Nombre = "Año (365 días)", FactorABase = 31536000.0 },
+                    new UnidadMedida { Nombre = "Década", FactorABase = 315360000.0 },
+                    new UnidadMedida { Nombre = "Siglo", FactorABase = 3153600000.0 }
+                }
+            };
+        }
+
+        private void ActualizarCombosUnidades()
+        {
+            if (comboBox3.SelectedItem == null) return;
+
+            string tipoSeleccionado = comboBox3.SelectedItem.ToString();
+            var listaUnidades = categorias[tipoSeleccionado];
+
+            comboBox1.DataSource = listaUnidades.ToList();
+            comboBox1.DisplayMember = "Nombre";
+
+            comboBox2.DataSource = listaUnidades.ToList();
+            comboBox2.DisplayMember = "Nombre";
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ActualizarCombosUnidades();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(textBox1.Text, out double cantidad))
+            {
+                MessageBox.Show("Por favor, ingrese un valor numérico válido.", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var unidadOrigen = (UnidadMedida)comboBox1.SelectedItem;
+            var unidadDestino = (UnidadMedida)comboBox2.SelectedItem;
+
+            if (unidadOrigen == null || unidadDestino == null) return;
+
+            double valorEnBase = cantidad * unidadOrigen.FactorABase;
+            double resultado = valorEnBase / unidadDestino.FactorABase;
+
+            lblResultado.Text = $"Resultado: {resultado:N4} {unidadDestino.Nombre}";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
